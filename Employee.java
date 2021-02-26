3d77ae867a24e361716335e4131ed84842c62ce4
unknown <amibagal02@gmail.com>
2021-02-26 17:21:43 +0530

    Employee file added1

---
+++ Employee.java
@@ -0,0 +1,31 @@
+public class employee
+{
+    int id;
+    double salary=0.0;
+
+    public employee(int Id)
+    {
+        this.Id = id;
+    }
+    public employee()
+    {
+    }
+
+    double calTax()
+    {
+        if(this.Salary>500000.00 && this.salary<1000000){
+            return this.salary*0.1;
+        }
+
+        else
+            if(this.salary>1000000.00 && this.Salary<1500000)
+            {
+                return this.Salary * 0.2;
+            }
+            else {
+                return this.Salary * 0.3;
+            }
+
+    }
+
+}
